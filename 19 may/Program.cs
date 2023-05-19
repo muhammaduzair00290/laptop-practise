f091ac0ccd83662919b8542fc0dd2f029f35abe9
muhammaduzair00290 <muhammaduzair12344321@gmail.com>
2023-05-19 10:17:38 -0700

    hello

--- 19 may/Program.cs
+++ 19 may/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine(": hello hfdfhfbdcbdjb");
+Console.WriteLine(" hyeee ");
+Console.WriteLine(" alinsd");
